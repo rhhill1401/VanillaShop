88ecabbe98e956ea937ee85712d9f5d23fe61e4b
Terry <rthill49@gmail.com>
2022-03-28 09:26:02 -0500

    error correction

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,32 @@
-import { getStorageItem, setStorageItem } from './utils.js';
-let store = [];
-const setupStore = () => {};
-const findProduct = () => {};
-export { store, setupStore, findProduct };
+//   ATTENTION!!!!!!!!!!!
+//   I SWITCHED TO PERMANENT DOMAIN
+//   DATA IS THE SAME JUST A DIFFERENT URL,
+//   DOES NOT AFFECT PROJECT FUNCTIONALITY
+
+const allProductsUrl = 'https://course-api.com/javascript-store-products';
+// temporary single product
+// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+const singleProductUrl =
+  'https://course-api.com/javascript-store-single-product';
+
+const getElement = selection => {
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  );
+};
+
+const formatPrice = () => {};
+
+const getStorageItem = () => {};
+const setStorageItem = () => {};
+
+export {
+  allProductsUrl,
+  singleProductUrl,
+  getElement,
+  formatPrice,
+  getStorageItem,
+  setStorageItem,
+};
